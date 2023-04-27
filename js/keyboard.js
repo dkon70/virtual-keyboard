5d9cd31a8e702b55ca2730e0999ca9cb77580737
Dmitry Konev <dkon70@inbox.ru>
2023-04-28 01:32:31 +0700

    feat: add basic layout and functionality

---
+++ js/keyboard.js
@@ -0,0 +1,47 @@
+import { data, dataRightShift, dataLeftShift, dataCaps, dataCapsLeftShift } from './data.js'
+const kb = document.createElement('div')
+function createLayout () {
+  const wrapper = document.createElement('div')
+  wrapper.className = 'wrapper'
+
+  const h = document.createElement('h1')
+  h.className = 'h'
+  h.innerText = 'Key'
+  wrapper.append(h)
+
+  const form = document.createElement('form')
+  form.className = 'form'
+  const input = document.createElement('textarea')
+  input.cols = 67 
+  input.rows = 15
+  input.className = 'input'
+  form.append(input)
+  wrapper.append(form)
+
+  
+  kb.className = 'kb'
+  wrapper.append(kb)
+  const body = document.body;
+  body.append(wrapper);
+}
+
+function generateKeys (type) {
+  if (type === 'rightShift') {
+    kb.innerHTML = dataRightShift
+  }
+  if (type === 'leftShift') {
+    kb.innerHTML = dataLeftShift
+  }
+  if (type === 'normal') {
+    kb.innerHTML = data
+  }
+  if (type === 'caps') {
+    kb.innerHTML = dataCaps
+  }
+  if (type === 'leftShiftCaps') {
+    kb.innerHTML = dataCapsLeftShift
+  }
+  
+}
+
+export { createLayout, generateKeys }
---
+++ js/main.js
@@ -0,0 +1,155 @@
+import { createLayout, generateKeys } from './keyboard.js'
+
+createLayout()
+generateKeys('normal')
+let capsOn = false;
+let rShift = false;
+let lShift = false;
+
+const h = document.querySelector('.h')
+
+document.addEventListener('keydown', (event) => {
+  event.preventDefault()
+  h.innerText = ''
+  h.innerText = (event.code)
+})
+
+const input = document.querySelector('.input')
+input.value = ''
+
+document.addEventListener('keydown', keyDownHandler)
+
+function keyDownHandler (event) {
+  const key = document.querySelector(`.${event.code}`)
+  if (key && event.code !== 'CapsLock') {
+    key.classList.add('pressed')
+    if (event.code !== 'Backspace' &&
+        event.code !== 'Tab' &&
+        event.code !== 'AltLeft' &&
+        event.code !== 'AltRight' &&
+        event.code !== 'ControlLeft' &&
+        event.code !== 'ControlRight' &&
+        event.code !== 'ShiftLeft' &&
+        event.code !== 'ShiftRight' &&
+        event.code !== 'Delete' &&
+        event.code !== 'Enter' &&
+        event.code !== 'MetaLeft' &&
+        event.code !== 'ArrowLeft' &&
+        event.code !== 'ArrowUp' &&
+        event.code !== 'ArrowRight' &&
+        event.code !== 'ArrowDown' &&
+        event.code !== 'Caps') {
+      input.value += event.key
+      console.log(input.value)
+    } else if (event.code === 'Backspace') {
+      input.value = input.value.slice(0, -1)
+      console.log(input.value)
+    } else if (event.code === 'Tab') {
+      input.value += '    '
+    } else if (event.code === 'Delete') {
+      input.value = input.value.replace(input.value[0], '')
+      console.log(input.value)
+    }
+  } else if (event.code === 'CapsLock') {
+    if (capsOn === false) {
+      generateKeys('caps')
+      addClicks()
+      key.classList.toggle('pressed')
+      capsOn = true
+    } else {
+      generateKeys('normal')
+      addClicks()
+      key.classList.toggle('pressed')
+      capsOn = false
+    }
+  } 
+  if (event.code === 'ShiftLeft') {
+    generateKeys('leftShift')
+  }
+  if (event.code === 'ShiftRight') {
+    generateKeys('rightShift')
+  }
+}
+
+document.addEventListener('keyup', keyUpHandler)
+
+function keyUpHandler (event) {
+  const key = document.querySelector(`.${event.code}`)
+  if (key && event.code !== 'CapsLock') {
+    key.classList.remove('pressed')
+  }
+  if (key && event.code === 'ShiftLeft') {
+    generateKeys('normal')
+    addClicks()
+  }
+  if (key && event.code === 'ShiftRight') {
+    generateKeys('normal')
+    addClicks()
+  }
+}
+
+function addClicks() {
+  const keys = document.querySelectorAll('.keys')
+
+  keys.forEach((key) => {
+    key.addEventListener('mousedown', function mouseDown() {
+      h.innerText = ''
+      h.innerText = (key.classList[0])
+      if (key.innerHTML !== 'Caps' && 
+        key.innerHTML !== 'Ctrl' && 
+        key.innerHTML !== 'Alt' &&
+        key.innerHTML !== 'Shift' &&
+        key.innerHTML !== 'Win' &&
+        key.innerHTML !== '⇄' &&
+        key.innerHTML !== '⤶' &&
+        key.innerHTML !== '⇐' &&
+        key.innerHTML !== '_____' &&
+        key.innerHTML !== 'Del' &&
+        key.innerHTML !== '⇐' &&
+        key.innerHTML !== '⇑' &&
+        key.innerHTML !== '⇓' &&
+        key.innerHTML !== '⇒') {
+          input.value += key.innerText
+      }
+      
+
+      if (key && key.classList[0] !== 'CapsLock' && 
+        key.classList[0] !== 'ShiftRight' &&
+        key.classList[0] !== 'ShiftLeft') {
+        key.classList.add('pressed')
+      } else {
+        key.classList.toggle('pressed')
+        if (key.classList[0] === 'CapsLock') {
+          if (!capsOn) {
+            generateKeys('caps')
+            addClicks()
+            capsOn = true
+          } else {
+            generateKeys('normal')
+            addClicks()
+            capsOn = false
+          }
+
+        }
+      }
+    })
+    key.addEventListener('mouseup', function mouseUp(event) {
+      event.preventDefault()
+      if (key && key.classList[0] !== 'CapsLock' && 
+          key.classList[0] !== 'ShiftRight' &&
+          key.classList[0] !== 'ShiftLeft') {
+        key.classList.remove('pressed')
+      }
+    })
+    key.addEventListener('mouseout', function mouseOut(event) {
+      event.preventDefault()
+      if (key && key.classList[0] !== 'CapsLock' && 
+        key.classList[0] !== 'ShiftRight' &&
+        key.classList[0] !== 'ShiftLeft') {
+        key.classList.remove('pressed')
+      }
+    })
+  })
+}
+
+addClicks()
